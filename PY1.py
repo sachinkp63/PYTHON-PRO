f97dc080bbee2ca0f11afcf8aa68df3f0a3b6921
sachinkp63 <sachinkumarpatel63@gmail.com>
2025-04-19 00:47:41 +0530

    Create PY1.py

---
+++ PY1.py
@@ -0,0 +1,14 @@
+# Printing hello world
+# Using print function
+# Printing string here
+print("Hello World!")
+#printing numeric
+print(38)
+#printing Boolean 
+print(True)
+
+
+# for comments we use "#" in the strating from where to give comments
+# comments means basically gives the description of the LOC(ine of code).....
+# as well as I mentioned earlier....
+# the comments will not show in the output section..... 
