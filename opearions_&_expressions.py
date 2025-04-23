f9612177e763914c868ce685721c65c94037dd28
sachinkp63 <sachinkumarpatel63@gmail.com>
2025-04-23 09:00:37 +0530

    Create opearions_&_expressions.py

---
+++ opearions_&_expressions.py
@@ -0,0 +1,31 @@
+#==================Operations===================
+
+# 1 => Arithematic Operations...
+# Additions(+)
+x = 3 
+y = 4
+print(x + y)
+# Subtraction(-)
+print(x - y)
+# Multiplication(*)
+print(x * y)
+# Division(/)
+print(x / y)
+# Modulus(%)
+print(x % y)
+
+# 2 => Comparison(Relations) Operations...
+# Equal(==)
+print(3==4)
+# notequal(!=)
+print(3 != 4)
+# Greater Than
+print(3 > 4)
+# Less Than
+print(3 < 4)
+# Greater Than equal
+print(3 >= 4)
+# Less Than equal
+print(3 <= 4)
+
+
