919651d3804a9aea6ccf78a67a8dc7d7222776e0
sachinkp63 <sachinkumarpatel63@gmail.com>
2025-04-24 19:47:46 +0530

    Create elgible_for_work.py

---
+++ conditional_statement/elgible_for_work.py
@@ -0,0 +1,5 @@
+age = int(input("Enter the age: "))
+if age <= 18 and age >= 60:
+    print(f"Elgibile for because his/her age is {age}")
+else:
+    print(f"Not Elgibile for because his/her age is {age}")
