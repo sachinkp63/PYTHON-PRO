618adbfcfaa03e2cc2d6fe956510e0e47c7d134c
sachinkp63 <sachinkumarpatel63@gmail.com>
2025-04-24 19:48:02 +0530

    Create valid&invalid.py

---
+++ conditional_statement/valid&invalid.py
@@ -0,0 +1,5 @@
+marks = int(input("Enter the marks: "))
+if marks <= 50 and marks >= 100:
+    print(f"He/She valid for the next round this marks: {marks}")
+else:
+    print(f"You are not valid for the next round marks: {marks}")
