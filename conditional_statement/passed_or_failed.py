27d892bbb6d0e1cd6530a409c7edbfaa971e45c0
sachinkp63 <sachinkumarpatel63@gmail.com>
2025-04-24 19:47:58 +0530

    Create passed_or_failed.py

---
+++ conditional_statement/passed_or_failed.py
@@ -0,0 +1,7 @@
+maths = int(input("Enter maths marks: "))
+physics = int(input("Enter physics marks: "))
+chemistry = int(input("Enter chemistry marks: "))
+if maths >= 45 and physics >= 45 and chemistry >= 45:
+    print("Passed")
+else:
+    print("Failed")
